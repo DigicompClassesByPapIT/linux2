fe025e26b78818594387822158037508f76772a4
chrichri <christophe.brun@papit.fr>
2024-09-08 22:31:33 +0200

    First release.

--- sqlite-hr.sh
+++ sqlite-hr.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash
+# Database
+readonly DB_NAME="hr.db"
+# Remove the database if it exists
+rm -f $DB_NAME
 # Check sqlite3 is installed
 if ! [ -x "$(command -v sqlite3)" ]; then
   echo 'Error: sqlite3 is not installed.' >&2
   exit 1
 fi
-# Create a database named hr.db abd employee table
-sqlite3 hr.db <<EOF
+# Create a database named hr.db add employee table
+sqlite3 $DB_NAME <<EOF
 CREATE TABLE employee (
   id INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
@@ -17,7 +21,7 @@ EOF
 
 # Insert data into the employee table from csv file employee.csv, --skip 1 to
 # skip the header
-sqlite3 hr.db <<EOF
+sqlite3 $DB_NAME <<EOF
 .mode csv
 .separator ;
 .import employee.csv --skip 1 employee
@@ -26,7 +30,7 @@ EOF
 # Query the employee table
 
 # Create table dept as for department
-sqlite3 hr.db <<EOF
+sqlite3 $DB_NAME <<EOF
 CREATE TABLE dept (
   id INTEGER PRIMARY KEY,
   label TEXT NOT NULL
@@ -35,7 +39,7 @@ EOF
 
 # Insert data into the dept table from csv file dept.csv, --skip 1 to skip
 # the header
-sqlite3 hr.db <<EOF
+sqlite3 $DB_NAME <<EOF
 .mode csv
 .separator ;
 .import dept.csv --skip 1 dept
@@ -44,7 +48,7 @@ EOF
 # Join the employee and dept tables based on the dept_id column
 # Print the name of the employees and the department label where the department
 # id is 0
-sqlite3 hr.db <<EOF
+sqlite3 $DB_NAME <<EOF
 .headers off
 .mode column
 .separator ROW "\n"
@@ -54,5 +58,3 @@ JOIN dept d
 ON e.dept_it = d.id
 WHERE d.id = 0;
 EOF
-# Drop database
-rm hr.db
